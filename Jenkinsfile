0827b4d03c84657c95ee8986cfa3ebf4eb7f1130
温昌寿 <wenchangshou@gmail.com>
2019-05-21 14:45:36 +0800

    Update

---
+++ Jenkinsfile
@@ -0,0 +1,7 @@
+#!/usr/bin/env groovy
+
+node('docker'){
+    String applicationName="basic-app"
+    String buildNumber="0.1${env.BUILD_NUMBER}"
+    
+}
--- main.go
+++ main.go
@@ -1,50 +1,25 @@
 package main
 
+// Import the fmt for formatting strings
+// Import os so we can read environment variables from the system
 import (
 	"fmt"
 	"os"
-
-	gocb "gopkg.in/couchbase/gocb.v1"
 )
 
-type BucketInterface interface {
-	Get(key string, value interface{}) (gocb.Cas, error)
-	Insert(key string, value interface{}, expiry uint32) (gocb.Cas, error)
-}
-
-type Database struct {
-	bucket BucketInterface
-}
-
-type Person struct {
-	Type      string `json:"type"`
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-}
-
-func (d Database) GetPersonDocument(key string) (interface{}, error) {
-	var data interface{}
-	_, err := d.bucket.Get(key, &data)
-	if err != nil {
-		return nil, err
+// create a function named getUserName that returns a single string
+func getUserName() string {
+	// On Linux and Mac the username is available as simply USER
+	username := os.Getenv("USER")
+	// check if we got an empty string and attempt to get USERNAME instead which would be available on Windows
+	if username == "" {
+		username = os.Getenv("USERNAME")
 	}
-	return data, nil
+	// Return whatever we received
+	return username
 }
 
-func (d Database) CreatePersonDocument(key string, data interface{}) (interface{}, error) {
-	_, err := d.bucket.Insert(key, data, 0)
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
-}
-
+// entrypoint function for our app
 func main() {
-	fmt.Println("Starting the application...")
-	var database Database
-	cluster, _ := gocb.Connect("couchbase://" + os.Getenv("DB_HOST"))
-	cluster.Authenticate(gocb.PasswordAuthenticator{Username: os.Getenv("DB_USER"), Password: os.Getenv("DB_PASS")})
-	database.bucket, _ = cluster.OpenBucket(os.Getenv("DB_BUCKET"), "")
-	fmt.Println(database.GetPersonDocument("8eaf1065-5bc7-49b5-8f04-c6a33472d9d5"))
-	database.CreatePersonDocument("blawson", Person{Type: "person", Firstname: "Brett", Lastname: "Lawson"})
+	fmt.Printf("Hello, %s\n", getUserName())
 }
